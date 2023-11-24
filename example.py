6425858ccda61bbecbe3bdee7e2be62c16eb5a1a
Prasad Fernando <prasadf@yahoo.com>
2023-11-24 00:07:37 +0000

    updates example.py

--- example.py
+++ example.py
@@ -1,7 +1,6 @@
 import time
 
 # This is an example python script to test merge conflicts
-time.sleep(3)
-print(f"Hello World!,Current time is {time.time()}")
-time.sleep(3)
-print("Second Change")
+time.sleep(5)
+print(f"Hello World!,Current time is {time.time(5)}")
+time.sleep(5)
